113daa8fb436d1edc53c7c009ba50ba804b90def
Andrew <andrew.j.zeller@gmail.com>
2016-08-14 00:01:22 -0700

    added queries

--- index.js
+++ index.js
@@ -3,28 +3,41 @@ var express = require('express');
 var moment = require('moment');
 var conf = require('./conf');
 
+// const userTime = require('userTime');             //  get current time of user
+// const userLatitude = require('userLatitude');     // get current latitude of user
+// const userLongitude = require('userLongitude');   // get current longitude of user
+// const userSearchRadius = require('userSearchRadius'); //get user search radius in meters (m)
+
 var WEEKDAYS = ["sunday","monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 var factual = new Factual(conf.factualKey, conf.factualSecret);
 var app = express();
 
+app.get('/', function(req, res) { //reuqesting location, timestamp, and searchRadius from frontend
+  res.send(req.query);
+});
+
 app.get('/factualInfos', function(req, serverResponse) {
+
+  var location = req.query.location || [ 37.7822671,-122.3934366 ];
+  var timestamp = req.query.timestamp || 1455139168717;
+  var searchRadius = req.query.searchRadius || 100;
 
   factualOptions = {
     geo: {
       "$circle": {
-        "$center": [ 37.782047, -122.391248],
-        "$meters": 100
+        "$center": location,
+        "$meters": searchRadius
       }
     }
   }
 
   factual.get('/t/restaurants-us',factualOptions, function (error, res) {
-    var deviceTime = new Date(1455139168717);
+    var deviceTime = new Date(timestamp);
     console.log(deviceTime);
     var openRestaurants = returnOpenRestaurants(res.data, deviceTime);
     console.log(openRestaurants);
-    serverResponse.json(openRestaurants);
+    serverResponse.json(openRestaurants); //server responds to the frontend with openRestaurants
   });
 });
 
